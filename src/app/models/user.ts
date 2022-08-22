16a354aba9ca77b293803c448821cf645be1ba04
Sandra Felicio <mjpfelicia@gmail.com>
2022-08-22 14:17:12 -0300

    add validaçao de email,e palavras restritas na pagina

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,5 @@
 export class User {
+    static email: string | undefined;
     constructor(
 
         public nome?: string,
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { faUser, faArrowLeft, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -10,6 +12,7 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  [x: string]: any;
   faArrowLeft = faArrowLeft;
   faUser = faUser;
   faEnvelope = faEnvelope;
@@ -17,60 +20,88 @@ export class LoginComponent implements OnInit {
   UserModel = new User();
   mensagem: string = ""
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  // salvarDados() {
-  //   console.log({ UserModel: this.UserModel })
-
-  //   this.userService.sigin(this.UserModel)
-  //     .subscribe(function (response) {
-  //       console.log({ response })
-
-  //     })
-  // }
 
 
-  validaLogin(): boolean {
-    if (
-      this.UserModel.nome === undefined || this.UserModel.nome === '' || 
-      this.UserModel.email === undefined || this.UserModel.email === '' ||
-      this.UserModel.password === undefined || this.UserModel.password === ''
-    ) { 
-      return false;
-    } else {
-      return true;
-    }
+  //  function validar email 
+  validateEmail(email: string | undefined): boolean {
+    const validarEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return validarEmail.test(String(email).toLowerCase());
   }
 
-  //Função de Login
-  signin() {
-    //fazer validação
-    if ( this.validaLogin() ) { 
-      console.log(this.UserModel);
-      this.userService.sigin(this.UserModel)
-        .subscribe(
-          {
-            next: (response) => {
-              console.log(response);
-              this.mensagem = `Logado com Sucesso! ${response.status} ${response.statusText}`
+  // function validar ataques SQL Injection
+  vadidatePalavrasProibidas(email: string | undefined): boolean {
+    const palavrasProibidas = ["select", "105", "or", "1 = 1", "=", "where", "delete", "insert", "create", "table"];
 
-            },
-            error: (e) => {
-              console.log("Não é possível encontrar o usuário ", e);
-              this.mensagem = `Não foi possível encontrar o usuário ${e.status} ${e.statusText}`
-            }
 
-          }
-        )
-
-    } else {
-
-      console.log(this.UserModel);
-      this.mensagem = "Preencha este campo corretamente."
+    if (!email) {
+      return false;
     }
+    //  procura palavra proibida e bloquia a primeira lavra que encontra 
+    const searchPalavraProibida = (palavra: string) => email.toLowerCase().search(palavra) > -1
+    const encontrada = palavrasProibidas.find(searchPalavraProibida)
+    console.log("Palavra proibida encontrada:", encontrada)
+    return !encontrada
+  }
+
+
+  validaLogin(user: User): boolean {
+
+    //validar campos 
+    if (
+      !user.nome ||
+      !user.email ||
+      !user.password
+    ) {
+      console.error('falta dados do usuario: ', { user });
+      return false;
+    }
+
+    if (!this.vadidatePalavrasProibidas(user.email)) {
+      console.error('email com palavras proibidas: ', this.UserModel);
+      return false;
+    }
+
+    if (!this.validateEmail(user.email)) {
+      console.error('email invalido: ', this.UserModel);
+      return false;
+    }
+
+    return true;
+
+  }
+
+
+  //fazer validação login e logar com API
+  signin() {
+    if (!this.validaLogin(this.UserModel)) {
+      console.log(this.UserModel);
+      this.mensagem = "preencher corretamente todos os campos."
+      return;
+    }
+
+    this.userService.sigin(this.UserModel)
+      .subscribe(
+        {
+          next: (response) => {
+            console.log(response);
+            this.mensagem = `Logado com Sucesso! ${response.status} ${response.statusText}`
+            this.router.navigate(['']);
+
+          },
+          error: (e) => {
+            console.error('Usuário não encontrado', e);
+            this.mensagem = `${e.error} ${e.status} ${e.statusText}`
+          }
+
+        }
+      )
+
   }
 
 }
